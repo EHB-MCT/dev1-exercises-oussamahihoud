554fdde161a650923c0e0493f883d1c0072493a9
oussama <oussama.hihoud@student.rhb.be>
2022-10-11 12:32:40 +0200

    1-2 Created a box with black and red lines

---
+++ exercises/1-functions/1-2.js
@@ -0,0 +1,58 @@
+"use strict"
+
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "red";
+context.moveTo(50, 50);
+context.lineTo(200, 200);
+context.stroke();
+
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "black";
+context.moveTo(200, 50);
+context.lineTo(200, 200);
+context.stroke();
+
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "black";
+context.moveTo(50, 200);
+context.lineTo(200, 200);
+context.stroke();
+
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "black";
+context.moveTo(50, 50);
+context.lineTo(200, 50);
+context.stroke();
+
+
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "black";
+context.moveTo(50, 50);
+context.lineTo(50, 200);
+context.stroke();
+
+
+context.beginPath();
+context.lineWidth = 5;
+context.strokeStyle = "red";
+context.moveTo(50, 200);
+context.lineTo(200, 50);
+context.stroke();
+
+
+
+
+
+
+

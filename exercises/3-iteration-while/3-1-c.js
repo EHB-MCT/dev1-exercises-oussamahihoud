bf1fed0986ac37775542fbaca309da63aba451b6
oussama <oussama.hihoud@student.rhb.be>
2022-10-19 16:17:47 +0200

    exercise3-1c finish

--- exercises/3-iteration-while/3-1-c.js
+++ exercises/3-iteration-while/3-1-c.js
@@ -1,23 +1,26 @@
 "use strict";
+
 import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
 
 draw();
 
 function draw() {
-    context.lineWidth = 2;
-    context.strokeStyle = "white";
-    context.fillStyle = "#3498DB";
-    context.fillRect(50, 50, 300, 300);
-    drawCross();
+	context.lineWidth = 2;
+	context.strokeStyle = "white";
+	context.fillStyle = "#3498DB";
+	context.fillRect(50, 50, 300, 300);
+
+	drawCross();
 }
 
 function drawCross() {
-    let i = 0;
-    while (i < 5) {
-        let step = ;
-        Utils.drawLine(50, step, 350, step);
-        Utils.drawLine(step, 50, step, 350);
-        i = ;
-    }
+	let i = 0;
+	while (i < 5) {
+		let step = 150;
+		let mid = 25;
+		Utils.drawLine(50, step + mid * i, 350, step + mid * i);
+		Utils.drawLine(step + mid * i, 50, step + mid * i, 350);
+		i++;
+	}
 }
